8ad38aa216acdf2f7d91ca5c0310d2be3ebd4d0c
panleiming <panleiming@linksaas.pro>
2024-01-30 12:25:55 +0800

    add call grpcutil sidecar with stdin data

--- src-tauri/src/minapp_api/min_app_grpc_plugin.rs
+++ src-tauri/src/minapp_api/min_app_grpc_plugin.rs
@@ -1,6 +1,6 @@
 use crate::minapp_api::min_app_plugin::get_min_app_perm;
 use tauri::{
-    api::process::Command,
+    api::process::{Command, CommandEvent},
     plugin::{Plugin, Result as PluginResult},
     AppHandle, Invoke, PageLoadPayload, Runtime, Window,
 };
@@ -40,6 +40,66 @@ async fn exec<R: Runtime>(
     return Ok(res.stdout.to_string());
 }
 
+#[tauri::command]
+async fn exec_pipe<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    args: Vec<String>,
+    pipe_data: String,
+) -> Result<String, String> {
+    //检查权限
+    let perm = get_min_app_perm(app_handle.clone(), window).await;
+    if perm.is_none() {
+        return Err("no perm".into());
+    }
+    let perm = perm.unwrap();
+    let perm = perm.net_perm;
+    if perm.is_none() {
+        return Err("no perm".into());
+    }
+    let perm = perm.unwrap();
+    if !perm.proxy_grpc {
+        return Err("no perm".into());
+    }
+
+    let cmd = Command::new_sidecar("grpcutil");
+    if cmd.is_err() {
+        return Err(cmd.err().unwrap().to_string());
+    }
+    let cmd = cmd.unwrap();
+    let cmd = cmd.args(args);
+    let res = cmd.spawn();
+    if res.is_err() {
+        return Err(res.err().unwrap().to_string());
+    }
+    let (mut rx, mut child) = res.unwrap();
+    if let Err(err) = child.write(pipe_data.as_bytes()) {
+        return Err(err.to_string());
+    }
+    if let Err(err) = child.write("\n".as_bytes()) {
+        return Err(err.to_string());
+    }
+    let mut ret = String::from("");
+    while let Some(event) = rx.recv().await {
+        if let CommandEvent::Stdout(line) = event {
+            ret += &line;
+            ret += "\n";
+        }
+    }
+    return Ok(ret);
+}
+
+#[tauri::command]
+async fn get_file(file_name: String) -> Result<String, String> {
+    let cache_path = crate::get_cache_dir();
+    if cache_path.is_none() {
+        return Err("no file".into());
+    }
+    let mut file_path = std::path::PathBuf::from(cache_path.unwrap());
+    file_path.push(file_name);
+    return Ok(file_path.to_string_lossy().to_string());
+}
+
 pub struct MinAppGrpcPlugin<R: Runtime> {
     invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync + 'static>,
 }
@@ -47,7 +107,7 @@ pub struct MinAppGrpcPlugin<R: Runtime> {
 impl<R: Runtime> MinAppGrpcPlugin<R> {
     pub fn new() -> Self {
         Self {
-            invoke_handler: Box::new(tauri::generate_handler![exec]),
+            invoke_handler: Box::new(tauri::generate_handler![exec, exec_pipe, get_file]),
         }
     }
 }
