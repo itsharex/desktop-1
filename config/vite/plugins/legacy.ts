13b560d05013e211232ac4e3293e35b930c10c37
panleiming <panleiming@linksaas.pro>
2024-02-19 15:16:26 +0800

    tmp commit

--- config/vite/plugins/legacy.ts
+++ config/vite/plugins/legacy.ts
@@ -2,6 +2,11 @@ import legacy from '@vitejs/plugin-legacy'
 
 export default function legacyConfig() {
   return legacy({
-    targets: ["chrome > 100"]
+    targets: ["> 1%",
+      "last 2 version",
+      "not dead",
+      "not ie <= 11",
+      "maintained node versions",
+      "not op_mini all"]
   })
 }
